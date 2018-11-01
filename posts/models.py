0a16ca62f9df9ce13e4274dfb2b63fc66566b9c3
Alex <harrison.alex117@gmail.com>
2018-11-01 09:40:39 -0700

    created a DB model called Post with DB field text.

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+#creating a model to store the textual
+#content of a message board post
+class Post(models.Model):
+    text = models.TextField()
