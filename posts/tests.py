bd4c7ff8c5de0c0db7323d025db5bb6c5a9cedc3
Alex <harrison.alex117@gmail.com>
2018-11-01 10:28:51 -0700

    addes a few more test files.

--- posts/tests.py
+++ posts/tests.py
@@ -1,5 +1,7 @@
 #importing sample database
 from django.test import TestCase
+from django.urls import reverse
+
 #importing Post to test
 from .models import Post
 
@@ -23,3 +25,21 @@ class PostModelTest(TestCase):
         expected_object_name = f'{post.text}'
         #assertEqual checks that our created entry matches
         self.assertEqual(expected_object_name, 'just a test')
+
+class HomePageViewTest(TestCase):
+#200 means throwing an HTTP 200 response
+    def setUp(self):
+        Post.objects.create(text='this is another test')
+
+    def test_view_url_exists_at_proper_location(self):
+        resp = self.client.get('/')
+        self.assertEqual(resp.status_code, 200)
+
+    def test_view_url_by_name(self):
+        resp = self.client.get(reverse('home'))
+        self.assertEqual(resp.status_code, 200)
+
+    def test_view_uses_correct_template(self):
+        resp = self.client.get(reverse('home'))
+        self.assertEqual(resp.status_code, 200)
+        self.assertTemplateUsed(resp, 'home.html')
